4282574e75e9721871e98dbcaf20a7f1c2ce8cb8
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-06-16 08:20:58 -0700

    project started

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { boolean } from 'yup';
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      id: '',
+      completed: boolean
+    };
+  }
+  
   render() {
+
     return (
       <div>
-        Todo App
+        <h1>To Do's</h1>
+        <ul>
+          <li>Study</li>
+          <li>Go to the gym</li>
+          <li>Cleaning home</li>
+        </ul>
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+        <button>Clear</button>
       </div>
     )
   }
