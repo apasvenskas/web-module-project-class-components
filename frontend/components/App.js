01c40acc0eabf03b18cb8fb812b5930db8328751
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-06-16 10:14:17 -0700

    work in progress

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,27 +1,61 @@
 import React from 'react'
 import { boolean } from 'yup';
+import Todo from './Todo';
+
+class TodoList extends React.Component {
+  render() {
+  return(
+    <ul>
+    {
+      this.props.todos.map(todo => {
+        // adding task comple to the todo.task 
+        return (<Todo todo={todo}/>)
+      })
+    }
+    {/* // <li>Study</li>
+    // <li>Go to the gym</li>
+    // <li>Cleaning home</li> */}
+  </ul>);
+  }
+}
 
 export default class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      name: '',
-      id: '',
-      completed: boolean
-    };
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          task: 'Study',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'Clean Home',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: 'Go to the gym',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
   }
   
-  render() {
-
+  render() { 
+    const { todos } = this.state;
+    
     return (
       <div>
         <h1>To Do's</h1>
-        <ul>
-          <li>Study</li>
-          <li>Go to the gym</li>
-          <li>Cleaning home</li>
-        </ul>
+        <TodoList todos={todos}/>
         <form>
           <input/>
           <button>Add</button>
